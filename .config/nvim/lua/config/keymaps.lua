20f3336d5e685ec5940c99d084752bf23b39d7a9
MuriloUnten <murilokenjiunten@gmail.com>
2023-07-20 16:10:40 -0300

    keymap: nvim change indentation

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,17 +5,24 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 
--- Normal mode --
-
 keymap("n", "<leader>s", ":w<cr>", opts) -- save file
 keymap("n", "<leader>h", ":noh<cr>", opts) -- clear highlights
 keymap("n", "<leader>c", ":bdelete<cr>", opts) -- close buffer
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
+
+-- buffer navigation
+keymap("n", "<S-h>", ":bprevious<cr>", opts)
+keymap("n", "<S-l>", ":bnext<cr>", opts)
+
+keymap("i", "jk", "<Esc>", opts)
+keymap("i", "kj", "<Esc>", opts)
+
 keymap("n", "<C-d>", "<C-d>zz", opts) -- pgdown && center
 keymap("n", "<C-u>", "<C-u>zz", opts) -- pgup && center
 keymap("n", "n", "nzzzv", opts) -- search next && center
 keymap("n", "N", "Nzzzv", opts) -- search previous && center
+
 
 -- window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -30,15 +37,6 @@ keymap("n", "<C-Down>", ":resize -2<cr>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
 
--- buffer navigation
-keymap("n", "<S-h>", ":bprevious<cr>", opts)
-keymap("n", "<S-l>", ":bnext<cr>", opts)
-
-
--- Insert mode --
-
-keymap("i", "jk", "<Esc>", opts)
-keymap("i", "kj", "<Esc>", opts)
 
 -- Move lines up and down
 keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
@@ -47,3 +45,8 @@ keymap("n", "<A-j>", "V:m '>+1<CR>gv=gv<Esc>", opts)
 keymap("n", "<A-k>", "V:m '<-2<CR>gv=gv<Esc>", opts)
 keymap("i", "<A-j>", "<Esc>V:m '>+1<CR>gv=gv<Esc>a", opts)
 keymap("i", "<A-k>", "<Esc>V:m '<-2<CR>gv=gv<Esc>a", opts)
+
+
+-- Change indentation && continue in VISUAL mode
+keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
