16e128f3fb1331bf6911a4b85b377e8e9fe53cea
MuriloUnten <murilokenjiunten@gmail.com>
2023-09-11 22:33:05 -0300

    qtile: change ... icon

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -92,7 +92,7 @@ keys = [
 groups = []
 
 group_names = ["1", "2", "3", "4", "5"]
-group_labels = ["", "󰾔", "󰭹", "󰎄", "󰇘"]
+group_labels = ["", "󰾔", "󰍩", "󰎄", "󰍻"]
 group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall"]
 
 for i in range(len(group_names)):
