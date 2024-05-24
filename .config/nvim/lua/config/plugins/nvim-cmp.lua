a5ba2d28ecd23338e121d04053059e7826be36db
Murilo Unten <murilo.unten@proton.me>
2024-05-24 10:01:53 -0300

    change autocompletion from entre to tab

--- .config/nvim/lua/config/plugins/nvim-cmp.lua
+++ .config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -29,7 +29,7 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-c>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({select = false}),
+                ["<Tab>"] = cmp.mapping.confirm({select = false}),
             },
             sources = cmp.config.sources({
                 {name = "nvim-lsp"},
