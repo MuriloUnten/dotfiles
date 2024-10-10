46e1002ce370c5d8acf6e3945c4274133f647dda
Murilo Unten <murilo.unten@proton.me>
2024-10-10 20:00:15 -0300

    nvim: add boilerplate

--- .config/nvim/lua/config/plugins/noice.lua
+++ .config/nvim/lua/config/plugins/noice.lua
@@ -10,6 +10,27 @@ return {
     },
     config = function()
         require("noice").setup({
+            cmdline = {
+                enabled = true,
+                view = "cmdline_popup",
+                opts = {},
+                ---@type table<string, CmdlineFormat>
+                format = {
+                    -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+                    -- view: (default is cmdline view)
+                    -- opts: any options passed to the view
+                    -- icon_hl_group: optional hl_group for the icon
+                    -- title: set to anything or empty string to hide
+                    cmdline = { pattern = "^:", icon = ">", lang = "vim" },
+                    search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+                    search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+                    filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+                    lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+                    help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+                    input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+                    -- lua = false, -- to disable a format, set to `false`
+                },
+            },
             lsp = {
                 override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
