1782a0465535aa18450d5712ffb7cfe01e496fe0
MuriloUnten <murilo.unten@proton.me>
2024-10-17 14:22:11 -0300

    nvim: change onedark theme configuration

--- .config/nvim/lua/config/plugins/onedark.lua
+++ .config/nvim/lua/config/plugins/onedark.lua
@@ -1,9 +1,43 @@
-All = {
+return {
     "navarasu/onedark.nvim",
     priority = 1000,
     config = function()
-        style = "dark"
-    end,
-}
+        require('onedark').setup({
+            style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+            transparent = false,
+            term_colors = true,
+            ending_tildes = false,
+            cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
-return All
+            -- toggle theme style ---
+            toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+            toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
+
+            -- Options are italic, bold, underline, none
+            -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+            code_style = {
+                comments = 'italic',
+                keywords = 'none',
+                functions = 'bold',
+                strings = 'none',
+                variables = 'none'
+            },
+
+            -- Lualine options --
+            lualine = {
+                transparent = false, -- lualine center bar transparency
+            },
+
+            -- Custom Highlights --
+            colors = {},     -- Override default colors
+            highlights = {}, -- Override highlight groups
+
+            -- Plugins Config --
+            diagnostics = {
+                darker = true,     -- darker colors for diagnostic
+                undercurl = true,  -- use undercurl instead of underline for diagnostics
+                background = true, -- use background color for virtual text
+            },
+        })
+    end
+}
